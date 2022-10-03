a3b7ebb46fc8822994ec3e6ad2ce911d78b8d969
Srujan <srujan@Srujans-MacBook-Air.local>
2022-10-03 09:33:50 +0530

    added nav component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate,useLoation } from 'react-router-dom'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as  PersonOutlineIcon} from '../assets/svg/personIcon.svg'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
